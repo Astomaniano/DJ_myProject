564ebe2245d3ee086cffb44fa640571b2601ef09
Astomaniano <alonso.astomaniano@gmail.com>
2024-10-02 15:21:17 +0400

    structure done
    without css and bootstrap

---
+++ My_site/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def galery(request):
+    return render(request, 'myapp/galery.html')
+
+def contact(request):
+    return render(request, 'myapp/contacts.html')
+
